7d071f96aa0b32c9a1c0e2cd137ed3df8ddb007e
yzhao72 <1549992628@qq.com>
2022-07-30 15:06:34 -0400

    new

---
+++ js/MainPage.js
@@ -0,0 +1,26 @@
+// 控制猫头鹰组件动态
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+
+
+
+
